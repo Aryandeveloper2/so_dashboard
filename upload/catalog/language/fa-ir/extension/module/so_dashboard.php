7382b5f928a84a19555b1c291c976d18bb1373a5
AshkanMahdizadeh <ac.programmer.ashkan@gmail.com>
2025-09-06 15:39:36 +0330

    initial

---
+++ upload/catalog/language/fa-ir/extension/module/so_dashboard.php
@@ -0,0 +1,54 @@
+<?php
+// Text
+$_['text_register']    = 'ثبت‌نام';
+$_['text_login']       = 'ورود';
+$_['text_logout']      = 'خروج';
+$_['text_forgotten']   = 'فراموشی رمز';
+$_['text_account']     = 'پیشخوان';
+$_['text_edit']        = 'ویرایش حساب کاربری';
+$_['text_password']    = 'تغییر رمز عبور';
+$_['text_address']     = 'آدرس‌ها';
+$_['text_wishlist']    = 'مورد علاقه';
+$_['text_order']       = 'سفارش‌ها';
+$_['text_download']    = 'دانلودها';
+$_['text_reward']      = 'امتیازهای جایزه';
+$_['text_return']      = 'برگشت شده‌ها';
+$_['text_transaction'] = 'تراکنش‌ها';
+$_['text_newsletter']  = 'خبرنامه';
+$_['text_recurring']   = 'پرداخت‌های دوره‌ای';
+
+$_['text_my_dashboard']     = 'پیشخوان';
+$_['text_headingProfile']   = 'حساب من';
+$_['text_headingOrder']     = 'سفارش';
+$_['text_editProfile']      = 'ویرایش پروفایل';
+$_['text_latestOrders']     = 'آخرین سفارش‌ها';
+$_['text_viewallOrder']     = 'نمایش همه برچسب سفارش';
+$_['text_totalOrder']       = 'جمع سفارش';
+$_['text_totalWishlist']    = 'مورد علاقه‌ها';
+$_['text_totalDownload']    = 'دانلودها';
+$_['text_totalTransactions']= 'تراکنش‌ها';
+
+$_['text_review']           = 'دیدگاه‌ها';
+$_['text_setting']          = 'تنظیمات';
+$_['text_subscription']     = 'اشتراک ویژه';
+
+$_['entry_profileImage']    = 'عکس پروفایل';
+$_['entry_upload']          = 'آپلود فایل';
+
+// Column
+$_['column_order_id']       = 'شماره سفارش';
+$_['column_customer']       = 'مشتری';
+$_['column_product']        = 'تعداد کالاها';
+$_['column_name']           = 'نام کالا';
+$_['column_model']          = 'مدل';
+$_['column_quantity']       = 'تعداد';
+$_['column_price']          = 'قیمت';
+$_['column_total']          = 'جمع کل';
+$_['column_action']         = 'عملیات';
+$_['column_date_added']     = 'تاریخ درج';
+$_['column_status']         = 'وضعیت';
+$_['column_comment']        = 'نظر';
+
+$_['text_wishlist']     = 'علاقه‌مندی‌ها';
+$_['text_empty_orders'] = 'هیچ موردی جهت نمایش وجود ندارد.';
+$_['text_totalAmount']  = 'اعتبار کیف پول';
